1d713e8764a6e6a642a06101b914ae338d9f3989
Rickard Laurin <rickard.laurin@gmail.com>
2020-09-08 17:55:51 +0200

    chore: try without open handler

--- src/index.ts
+++ src/index.ts
@@ -8,32 +8,27 @@ import {
   workspace,
 } from 'coc.nvim'
 import { resolve } from 'path'
-import { TextDocument } from 'vscode-languageserver-textdocument'
 
 export function activate(context: ExtensionContext) {
   const module = resolve(context.extensionPath, 'src', 'lsp', 'server.js')
   const outputChannel: OutputChannel = workspace.createOutputChannel('rescript')
 
-  async function didOpenTextDocument(_document: TextDocument): Promise<void> {
-    const serverOptions: ServerOptions = {
-      run: { module, transport: TransportKind.ipc },
-      debug: { module },
-    }
-
-    const clientOptions: LanguageClientOptions = {
-      documentSelector: [{ language: 'rescript', scheme: 'file' }],
-      outputChannel,
-    }
-
-    const client = new LanguageClient(
-      'rescript',
-      'ReScript',
-      serverOptions,
-      clientOptions
-    )
-
-    client.start()
+  const serverOptions: ServerOptions = {
+    run: { module, transport: TransportKind.ipc },
+    debug: { module },
   }
 
-  workspace.onDidOpenTextDocument(didOpenTextDocument)
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ language: 'rescript', scheme: 'file' }],
+    outputChannel,
+  }
+
+  const client = new LanguageClient(
+    'rescript',
+    'ReScript',
+    serverOptions,
+    clientOptions
+  )
+
+  client.start()
 }
