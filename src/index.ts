9899004f341b108178187ca02b00fd8d2d8692e9
Rickard Laurin <rickard.laurin@gmail.com>
2020-09-08 16:28:08 +0200

    fix: add typescript, add webpack

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import {
+  ExtensionContext,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  services,
+  workspace,
+} from 'coc.nvim'
+
+export const activate = (context: ExtensionContext) => {
+  const serverOptions: ServerOptions = {
+    module: context.asAbsolutePath('./src/lsp/server.js'),
+    args: ['--node-ipc'],
+  }
+
+  const documentSelector = [{ language: 'rescript', scheme: 'file' }]
+  const synchronize = {
+    configurationSection: 'rescript',
+  }
+  const clientOptions: LanguageClientOptions = { documentSelector, synchronize }
+
+  const languageClient = new LanguageClient(
+    'rescript',
+    'ReScript',
+    serverOptions,
+    clientOptions
+  )
+
+  context.subscriptions.push(services.registLanguageClient(languageClient))
+
+  workspace.showMessage('ReScript installed')
+}
